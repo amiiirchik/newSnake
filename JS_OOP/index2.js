7f5be369c04e62a3b8d880bad55f8abf06120ba0
AdiosSySni <adiossysni@gmail.com>
2022-03-24 22:47:06 +0200

    Работающий код Сергея

---
+++ JS_OOP/index2.js
@@ -0,0 +1,64 @@
+"use strict"
+
+class Snake {
+    #player = {
+        name: null,
+        skin: null,
+        keys: {
+            topKey: null,
+            bottomKey: null,
+            leftKey: null,
+            rightKey: null
+        }
+    }
+
+    constructor(player) {
+        this.#player = player;
+    }
+
+    getShow() {
+       return console.log(this.#player.name,
+        this.#player.skin,
+        this.#player.keys.topKey);
+    }
+
+}
+
+let snake = new Snake(  {
+                        name: 'Alex', 
+                        skin:'red', 
+                        keys: {
+                            topKey:37, 
+                            bottomKey:38, 
+                            leftKey:39, 
+                            rightKey:40}
+                        }
+                    );
+
+
+snake.getShow();
+
+console.log(snake);
+
+
+
+
+//    const player = {
+//         name: null,
+//         skin: null,
+//         keys: {
+//             topKey: null,
+//             bottomKey: null,
+//             leftKey: null,
+//             rightKey: null
+//         }
+
+
+//     }
+
+//     player.name = 'Алекс'
+
+    
+
+// console.log(player);
+
