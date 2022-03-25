c460ae007ab7fec1406c4000acaed4e550a0bc07
amiiirchik <ragimovamir@yandex.ru>
2022-03-25 17:58:49 +0200

    updated .js

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ class Apple extends Items{
 }
 
 class Banana extends Items{
+    // Расписать таймер и секунды
     increaseTime(timer){
         date.setSeconds(seconds + 10);
     }
@@ -53,11 +54,37 @@ class Amanita extends Items{
 }
 // Змея 
 class Snake{
-    constructor(x, y, snakeHeadX, snakeHeadY){
-        this.x = x * cellSize;
-        this.y = y * cellSize;
+    // #player = {
+    //     name: null,
+    //     skin: null,
+    //     keys: {
+    //         topKey: null,
+    //         bottomKey: null,
+    //         leftKey: null,
+    //         rightKey: null
+    //     }
+    // }
+    constructor(x, y, snakeHeadX, snakeHeadY){ 
+        this.x = x;
+        this.y = y;
         this.snakeHeadX = snakeHeadX;
         this.snakeHeadY = snakeHeadY;
+    }
+
+    getX(){
+        return this.x;
+    }
+
+    getY(){
+        return this.y;
+    }
+
+    getSnakeHeadY(){
+        return ((this.snakeHeadY * this.y) + cellSize);
+    }
+
+    getSnakeHeadX(){
+        return ((this.snakeHeadX * this.x) + cellSize);
     }
 }
 
@@ -67,11 +94,21 @@ class Player1 extends Snake {
     Move(e){
         if (e.keyCode == 37 && pos != 'right'){
             pos = 'left';
+            snake1.x += 32;
             console.log(pos);  
         }  
-        if (e.keyCode == 38 && pos != 'down') pos = 'up';
-        if (e.keyCode == 39 && pos != 'left') pos = 'right';
-        if (e.keyCode == 40 && pos != 'up') pos = 'down';
+        if (e.keyCode == 38 && pos != 'down'){
+            pos = 'up';
+            console.log(pos);
+        }
+        if (e.keyCode == 39 && pos != 'left'){
+            pos = 'right';
+            console.log(pos);
+        }
+        if (e.keyCode == 40 && pos != 'up'){
+            pos = 'down';
+            console.log(pos);
+        }
     }
 
     getName(){
@@ -84,12 +121,21 @@ class Player2 extends Snake{
 
     Move(e){
         if (e.keyCode == 65 && pos != 'right'){
-            pos = 'left';
+            pos = 'a';
             console.log(pos);  
         }  
-        if (e.keyCode == 87 && pos != 'down') pos = 'up';
-        if (e.keyCode == 68 && pos != 'left') pos = 'right';
-        if (e.keyCode == 83 && pos != 'up') pos = 'down';
+        if (e.keyCode == 87 && pos != 'down'){
+            pos = 'w';
+            console.log(pos);
+        }
+        if (e.keyCode == 68 && pos != 'left'){
+            pos = 'd';
+            console.log(pos);
+        }
+        if (e.keyCode == 83 && pos != 'up'){
+            pos = 's';
+            console.log(pos);
+        } 
     }
 
     getName(){
@@ -97,17 +143,9 @@ class Player2 extends Snake{
     }
 }
 
-// function gameLoop(){
-//     let bg = new Image(); bg.src = "img/bg.jpg";
-//     context.drawImage(bg, 0, 0);
-//     let apple = new Apple('img/apple.png'); apple.Spawn();
-//     let banana = new Banana('img/banana.png'); banana.Spawn();
-//     let rock = new Rock('img/stone.png'); rock.Spawn(); let rock2 =  new Rock('img/stone.png'); rock2.Spawn(); let rock3 =  new Rock('img/stone.png'); rock3.Spawn();
-//     let mushroom = new Amanita('img/mushroom.png'); mushroom.Spawn();
-// }
 
-function gameConfig()
-{
+// Конфиг игры -- запускается один раз по вызову
+function gameConfig(){
     let bg = new Image(); bg.src = "img/bg.jpg";
     context.drawImage(bg, 0, 0);
     let singleBanana = new Banana('img/banana1.png');  singleBanana.Spawn();
@@ -115,25 +153,44 @@ function gameConfig()
     let tripleBanana = new Banana('img/banana3.png');  tripleBanana.Spawn();
     let mushroom = new Amanita('img/mushroom.png');  mushroom.Spawn();
 
-
-    for (let i = 0; i <= GetRandom(0, 3); i++)
-    {
-        let apple = new Apple('img/apple.png');
-        apple.Spawn();
+    for (let i = 0; i <= GetRandom(0, 3); i++){
+        let apple = new Apple('img/apple.png'); apple.Spawn();
     }
 
-    for (let i = 0; i <= GetRandom(0, 10); i++)
-    {
-        let rock = new Rock('img/stone.png');
-        rock.Spawn();
+    for (let i = 0; i <= GetRandom(0, 10); i++){
+        let rock = new Rock('img/stone.png'); rock.Spawn();
     }
-} setTimeout(gameConfig, 0);
 
-let snake1 = new Player1(3,4);
+} gameConfig();
+
+// Спавн змеи
+let snake1 = new Player1(3,7);
+console.log(snake1);
 let snake2 = new Player2(2,8);
 
+// Отслеживание нажатия у каждого игрока
 document.addEventListener('keydown', (e) => snake1.Move(e));   
 document.addEventListener('keydown', (e) => snake2.Move(e)); 
 
-function GetRandom(min, max){ return Math.floor(Math.random() * (max - min) + min); }
 
+// Луп игры
+function gameLoop(){
+    for (let i = 0; i < Object.keys(snake1).length; i++) {
+        context.fillStyle = "red";
+        context.fillRect(snake1.getX(), snake1.getY(), cellSize, cellSize);
+        
+        let newHead = {
+            x: snake1.getX(),
+            y: snake1.getY()
+        }
+
+        Array.from(snake1).unshift(newHead);
+    }
+
+    Array.from(snake1).pop();
+    console.log('gameloop');
+
+} setInterval(gameLoop, 100);
+
+
+function GetRandom(min, max){ return Math.floor(Math.random() * (max - min) + min); }
